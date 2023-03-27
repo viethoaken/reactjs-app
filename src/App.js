645ee0c0f211699b6c17e99e1cee981a7c1ef46b
hoadong <viethoaken@gmail.com>
2023-03-27 14:33:44 +0700

    Added router for Todo and Album component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 // import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import TodoFeature from './features/Todo';
+import AlbumFeature from './features/Album';
 // import ColoBox from './components/ColorBox';
 // import Counter from './components/Counter';
-// import AlbumFeature from './features/Album';
 
 function App() {
 
   return (
-        <div className='Apptodo'>
-          <TodoFeature />
-          {/* <AlbumFeature/> */}
-          {/* <ColoBox/>
-          <Counter/>
-          <AlbumFeature/> */}
-        </div>
+    <div className='App'>
+      Home page
+      <Routes>
+        < Route path='/todos' element = {<TodoFeature/>} />
+        < Route path='/albums' element = {<AlbumFeature/>} />
+      </Routes>
+      
+    </div>
   );
 }
 
